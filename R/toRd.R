4f54672a087463d77913133f7001a0cb29b3f02b
Andrew Redd <Andrew.Redd@hsc.utah.edu>
2019-01-19 13:06:05 -0700

    Added Examples

--- R/toRd.R
+++ R/toRd.R
@@ -12,6 +12,22 @@ setOldClass("shiny.tag")
 #' * Root generic: \code{\link[Rd]{toRd}}
 #' * Methods: \code{\link{toRd.shiny.tag-methods}}
 #' * Generators: \code{\link{toRd.shiny.tag-generators}}
+#' @examples
+#' toRd(htmltools::a("some text", href="https://r-project.org"))
+#' toRd(htmltools::tags$abbr("GPL"))
+#' toRd(htmltools::code("plot(rnorm(100))"))
+#' toRd(htmltools::tags$dfn("abc"))
+#' toRd(htmltools::tags$dl( htmltools::tags$dt("term1")
+#'                        , htmltools::tags$dd("definition 1.")
+#'                        , htmltools::tags$dt("term2")
+#'                        , htmltools::tags$dd("definition 2.")
+#'                        ))
+#' toRd(htmltools::tags$ol( htmltools::tags$li("First")
+#'                        , htmltools::tags$li("Second")
+#'                        ))
+#' toRd(htmltools::tags$ul( htmltools::tags$li("First")
+#'                        , htmltools::tags$li("Second")
+#'                        ))
 toRd.shiny.tag <-
 function(obj, ...){
     UseMethod('toRd.shiny.tag', cl(obj, html_get_type(obj)))
